a8071bf12cb47e327f91a60800b5a0178a941f96
Лысов Глеб <lysovg9@gmail.com>
2023-01-22 22:21:40 +0300

    Added: memory mapped test

--- core/tests/memorymapped.cpp
+++ core/tests/memorymapped.cpp
@@ -4,25 +4,41 @@
 #include <boost/ut.hpp>
 
 #include "cpu/cpu.hpp"
-#include "memory/memorymapper.hpp"
 #include "cpu/instructions.hpp"
+#include "memory/IMemoryMappedDevice.hpp"
+#include "memory/memorymapper.hpp"
 
 static plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
 
 // TODO: somewhat make it
-class MockScreenDevice
+class MockScreenDevice final : public IMemoryMappedDevice {
+public:
+	inline static int count;
+
+	std::uint16_t getUint16(std::uint16_t) const { return 0;}
+	std::uint8_t getUint8(std::uint16_t) const { return 0; }
+	void setUint16(std::uint16_t, std::uint16_t value) {
+		if (value == 1) {
+			count++;
+		} else if ( value == 2) {
+			count--;
+		}
+	}
+	void setUint8(std::uint16_t, std::uint8_t) {}
+};
 
 int main() {
 	plog::init(plog::debug, &consoleAppender);
 	using namespace boost::ut;
 
 	"simple mapped test"_test = [] {
+		MockScreenDevice::count = 0;
 		auto MM = std::make_unique<MemoryMapper>();
 
 		auto mem = std::make_unique<Memory>(256 * 256);
 		auto writableMemory = mem->makeWritable();
 
-		auto screenDevice = std::make_unique<ScreenDevice>();
+		auto screenDevice = std::make_unique<MockScreenDevice>();
 
 		MM->map({std::move(mem), 0, 0xffff});
 		MM->map({std::move(screenDevice), 0x3000, 0x30ff, true});
@@ -31,18 +47,24 @@ int main() {
 
 		auto cpu = CPU::CPU(std::move(MM));
 
-		writableMemory[i++] = Instructions::MOV_LIT_REG;
-		writableMemory[i++] = 0x12;
-		writableMemory[i++] = 0x34;
-		writableMemory[i++] = CPU::R1;
+		writableMemory[i++] = Instructions::MOV_LIT_MEM;
+		writableMemory[i++] = 0x00;
+		writableMemory[i++] = 0x01;
+		writableMemory[i++] = 0x30;
+		writableMemory[i++] = 0x01;
 
-		writableMemory[i++] = Instructions::MOV_LIT_REG;
-		writableMemory[i++] = 0xAB;
-		writableMemory[i++] = 0xCD;
-		writableMemory[i++] = CPU::R2;
+		writableMemory[i++] = Instructions::MOV_LIT_MEM;
+		writableMemory[i++] = 0x00;
+		writableMemory[i++] = 0x02;
+		writableMemory[i++] = 0x30;
+		writableMemory[i++] = 0x01;
 
 		writableMemory[i++] = Instructions::HLT;
 
-		cpu.run();
+		cpu.step();
+		expect(MockScreenDevice::count == 1_i);
+		
+		cpu.step();
+		expect(MockScreenDevice::count == 0_i);
 	};
 }
