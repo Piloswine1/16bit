fed30b6c2ebcd90ee7663e4d25f194ba9353f70d
Лысов Глеб <lysovg9@gmail.com>
2023-01-25 16:00:26 +0300

    Added: fallback to lexing regs as idents

--- asm/src/lexer.rs
+++ asm/src/lexer.rs
@@ -29,23 +29,28 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn tokenize(input: &str) -> impl Iterator<Item = Token> + '_ {
-        let mut lexer = Lexer::new(input);
+        let mut lines = input.lines();
+        let mut lexer = Lexer::new(lines.next().unwrap());
         std::iter::from_fn(move || {
             let token = lexer.parse_token();
-            println!("parsed: {:?}", token);
-            
+            // println!("parsed: {:?}", token);
+
             if token != Token::EOF {
                 Some(token)
+            } else if let Some(line) = lines.next() {
+                lexer = Lexer::new(line);
+                Some(Token::EOL)
             } else {
                 None
             }
         })
     }
 
+    // TODO: think of reg lexing
     pub fn parse_token(&mut self) -> Token {
         let first_char = self.cursor.next();
         println!("to parse: {:?}", first_char);
-        
+
         match first_char {
             Some(x) if x.is_whitespace() => self.eat_whitespace(),
             Some('&') => self.parse_mem(),
@@ -55,35 +60,42 @@ impl<'a> Lexer<'a> {
             Some(')') => Token::CloseParen,
             Some('[') => Token::OpenBracket,
             Some(']') => Token::CloseBracket,
+            Some('!') => Token::Neg,
+            Some(':') => Token::Colon,
             Some('+') => Token::Plus,
             Some('*') => Token::Star,
             Some('-') => Token::Minus,
-            Some('r') => match self.cursor.next() {
-                Some('1') => Token::Reg(crate::common::Regs::R1),
-                Some('2') => Token::Reg(crate::common::Regs::R2),
-                Some('3') => Token::Reg(crate::common::Regs::R3),
-                Some('4') => Token::Reg(crate::common::Regs::R4),
-                Some('5') => Token::Reg(crate::common::Regs::R5),
-                Some('6') => Token::Reg(crate::common::Regs::R6),
-                Some('7') => Token::Reg(crate::common::Regs::R7),
-                Some('8') => Token::Reg(crate::common::Regs::R8),
-                None => Token::Ident("r".into()),
-                // XXX: can be r0 unvalid register, mb can use as variable
-                _ => unimplemented!(),
-            },
-            Some(c) if c.is_alphabetic() => match self.parse_ident() {
-                Ok(x) => {
-                    let mut ident = String::from(c);
-                    ident.push_str(x.as_str());
-                    Token::Ident(ident)
-                }
-                Err(_) => Token::InvalidIdent,
-            },
+            // Some('r') => match self.cursor.next() {
+            //     Some('1') => Token::Reg(crate::common::Regs::R1),
+            //     Some('2') => Token::Reg(crate::common::Regs::R2),
+            //     Some('3') => Token::Reg(crate::common::Regs::R3),
+            //     Some('4') => Token::Reg(crate::common::Regs::R4),
+            //     Some('5') => Token::Reg(crate::common::Regs::R5),
+            //     Some('6') => Token::Reg(crate::common::Regs::R6),
+            //     Some('7') => Token::Reg(crate::common::Regs::R7),
+            //     Some('8') => Token::Reg(crate::common::Regs::R8),
+            //     None => Token::Ident("r".into()),
+            //     // XXX: can be r0 unvalid register, mb can use as variable
+            //     _ => unimplemented!(),
+            // },
+            Some(c) if c.is_alphabetic() => self.parse_ident(c),
             None => Token::EOF,
             // XXX: dunno what can happen
             _ => unimplemented!(),
         }
     }
+
+    /// Implicitly we ate 'r'
+    // pub fn try_parse_reg(&mut self, first_char: char) -> Token {
+    //     match self.cursor.peek() {
+    //         Some('_') => {
+    //             self.cursor.next();
+    //
+    //         },
+    //         None => Token::Ident("r".into()),
+    //         Some(_) => self.parse_ident(first_char),
+    //     }
+    // }
 
     pub fn parse_hex(&mut self) -> Result<u16> {
         let mut hex = String::new();
@@ -122,16 +134,17 @@ impl<'a> Lexer<'a> {
         Token::Whitespace
     }
 
-    fn parse_ident(&mut self) -> Result<String> {
-        let mut ident = String::new();
+    fn parse_ident(&mut self, first_char: char) -> Token {
+        let mut ident = String::from(first_char);
         while let Some(c) = self.cursor.peek() {
+            // println!("peek: {:?}", c);
             if !is_valid_id_continue(c) {
                 // TODO: handle invalid idents
                 break;
             }
             ident.push(self.cursor.next().unwrap());
         }
-        Ok(ident)
+        Token::Ident(ident)
     }
 }
 
@@ -146,7 +159,30 @@ fn parse_mov() {
             Token::Lit(10),
             Token::Comma,
             Token::Whitespace,
-            Token::Reg(crate::common::Regs::R2),
+            Token::Ident("r2".into()),
+        ]
+    );
+}
+
+#[test]
+fn parse_multiline() {
+    let tokens: Vec<_> = Lexer::tokenize("mov $10, r2\nmov &10, acc").collect();
+    assert_eq!(
+        tokens,
+        vec![
+            Token::Ident("mov".into()),
+            Token::Whitespace,
+            Token::Lit(10),
+            Token::Comma,
+            Token::Whitespace,
+            Token::Ident("r2".into()),
+            Token::EOL,
+            Token::Ident("mov".into()),
+            Token::Whitespace,
+            Token::Mem(10),
+            Token::Comma,
+            Token::Whitespace,
+            Token::Ident("acc".into()),
         ]
     );
 }
